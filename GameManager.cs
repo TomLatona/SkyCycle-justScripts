390d80e6bc4323cc93be40af66df1ab4522177ac
tom <latona.tom1@gmail.com>
2021-02-28 22:27:37 -0500

    Release 1.0

---
+++ GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public float restartDelay = 1f;
+    public GameObject gameOverUI;
+    public GameObject fs;
+    bool gameOv=false;
+
+    void Restart(){
+        //restart current level
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void CompleteLevel(float playerFinalPosition){ //activates on collision
+        Invoke("gameOverUIcont", restartDelay); //delay after collision before gameover screen
+        fs.GetComponent<finalScore>().scoreNumber = playerFinalPosition;
+    }
+
+    void gameOverUIcont(){
+        //enable game over ui, sets gameOv bool to true
+        gameOverUI.SetActive(true);
+        gameOv = true;
+    }
+
+    public void FixedUpdate(){
+        if(gameOv==true){
+            if (Input.GetKey("r")){
+                Restart();
+            }
+            if(Input.GetKey("q")){
+                SceneManager.LoadScene("MenuLevel");
+            }
+        }
+    }
+}
---
+++ movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class movement : MonoBehaviour{
+    
+    public Rigidbody rb;
+    public float forwardForce = 1700f;
+    public float sidwaysForce = 5500f;
+
+    void FixedUpdate(){
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); //sync with framerate(Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidwaysForce * Time.deltaTime, 0, 0);
+        }
+        
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidwaysForce * Time.deltaTime, 0, 0); 
+        }  
+    }
+}
